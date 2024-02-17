468e5293e251bbc30b39a2700c01b3b9b26c22e5
nahakritesh06 <nahakritesh06@gmail.com>
2024-02-18 00:26:06 +0530

    Basic algo

---
+++ src/main/java/com/dsa/recursion/basic/P10DecimalToBinary.java
@@ -0,0 +1,11 @@
+package com.dsa.recursion.basic;
+
+public class P10DecimalToBinary {
+    public static void main(String[] args) {
+        System.out.println(convert(8));
+    }
+    static int convert(int n){
+        if (n==0) return 0;
+        return (n%2+10*convert(n/2));
+    }
+}
--- src/main/java/com/dsa/recursion/basic/P9CountOfDigit.java
+++ src/main/java/com/dsa/recursion/basic/P9CountOfDigit.java
@@ -1,6 +1,17 @@
 package com.dsa.recursion.basic;
 //count number of digit
 public class P9CountOfDigit {
+    public static void main(String[] args) {
+        System.out.println(countOfDigit(45));
+    }
+    static int count=0; // it should be declared as global else it won.t give correct ans
+    static int countOfDigit(int n){
+        if (n!=0){
+            count++;
+            countOfDigit(n/10);
+        }
+        return (count!=0)?count:1;
+    }
 
 
 }
