bce8c64645c39b480a3b275e3bd5c9ae833b4556
nahakritesh06 <nahakritesh06@gmail.com>
2024-02-18 23:24:29 +0530

    Bubble Sort

---
+++ src/main/java/com/dsa/sorting/P01BubbleSort.java
@@ -0,0 +1,28 @@
+package com.dsa.sorting;
+
+import java.util.Arrays;
+
+public class P01BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 1, 46, 73, 23, 12, 45};
+        System.out.println("Before Sorting " + Arrays.toString(arr));
+        bubbleSortAsc(arr);
+        System.out.println("After  Sorting " + Arrays.toString(arr));
+    }
+
+    private static void bubbleSortAsc(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j=0;j<arr.length-i-1;j++){
+                if (arr[j]<arr[j+1]){ // change this condition for ascending
+                    swapVal(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    private static void swapVal(int [] arr,int i, int j) {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
