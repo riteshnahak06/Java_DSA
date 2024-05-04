122ac4dbea5ed6e7723ab6fe1e153eba8dcfa10d
nahakritesh06 <nahakritesh06@gmail.com>
2024-05-04 23:59:15 +0530

    InsertElementIntoArray

--- src/main/java/com/dsa/arrays/medium/P03InsertElementIntoArray.java
+++ src/main/java/com/dsa/arrays/medium/P03InsertElementIntoArray.java
@@ -8,6 +8,8 @@ public class P03InsertElementIntoArray {
         System.out.println(Arrays.toString(insertAtLast(arr,60)));
         int[] arr2={23,45,321,44};
         System.out.println(Arrays.toString(insertAtFast(arr2,40)));
+        int[] arr3={23,45,321,44};
+        System.out.println(Arrays.toString(insetAtLocation(arr3,69,2)));
     }
     static int [] insertAtLast(int [] arr,int element){
         int i,result[]=new int[arr.length+1];
@@ -23,6 +25,17 @@ public class P03InsertElementIntoArray {
             result[i+1]=arr[i];
         }
         result[0]=element;
+        return result;
+    }
+    static int [] insetAtLocation(int [] arr,int element,int location){
+        int i,k=0,result[]=new int[arr.length+1];
+        for (i=0;i<location;i++){
+           result[k++]=arr[i];
+        }
+        result[k++]=element;
+        for (i=location;i<arr.length;i++){
+            result[k++]=arr[i];
+        }
         return result;
     }
 
